1a480dff286bdcac789c2c8350c64080b07ee606
jsimplicio <hi@julessimplic.io>
2021-08-29 16:10:24 -0500

    New home

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const GlobalStyle = createGlobalStyle`
     padding-bottom: 48px;
   }
 `
-
 // styles
 const pageStyles = {
   alignItems: "center",
@@ -17,30 +16,45 @@ const pageStyles = {
   display: "flex",
   flexDirection: "column",
   flexWrap: "wrap",
-  fontFamily: "Tahoma, sans-serif, serif",
-  fontSize: 16,
+  fontFamily: "Times New Roman, serif",
+  fontSize: 18,
   justifyContent: "center",
-  marginTop: "1%",
+  margin: "16px 16px 8px 8px",
 }
+
 const articleStyles = {
   background: "#FFF",
   boxShadow: "1px 1px 0 0 rgba(255,255,255,1), inset 2px 2px 0 0 rgba(0,0,0,1)",
   maxWidth: '100%',
-  padding: "16px 16px 8px 16px",
-  margin: "2px 3px 2px 3px",
+  padding: '2%'
 }
+
 const sectionStyles = {
   background: "#CFCFCF",
   border: "1px solid #DDD",
   borderRadius: 2,
   boxShadow: "2px 2px 0 0 rgba(0,0,0,1), inset 2px 2px 0 0 rgba(255,255,255,1)",
   padding: 4, 
-  marginBottom: '2%',
-  maxWidth: 680,
+  marginBottom: 16,
+  maxWidth: 1124,
+  width: "100%",
+}
+
+const heroImages = {
+  display: "flex",
+  flexWrap: "wrap",
+  position: "relative",
+}
+
+const heroImage = {
+  borderRadius: 4,
+  flex: "400px",
+  marginRight: 48,
+  marginTop: 24,
 }
 
 const headingStyles = {
-  fontSize: 32,
+  fontSize: 24,
   marginTop: 16,
   marginBottom: 32,
 }
@@ -49,37 +63,43 @@ const paragraphStyles = {
   margin: 0,
   marginBottom: 24,
   padding: 0,
+  maxWidth: 668,
 }
-const listStyles = {
-  paddingLeft: 16,
-}
-const listItemStyles = {
-}
+// const listStyles = {
+//   paddingLeft: 16,
+// }
+// const listItemStyles = {
+// }
 const linkStyle = {
   color: "#232129",
 }
-const listDescription = {
-  color: "#232129",
-  display: "block",
-  marginBottom: 8,
-  padding: 0,
-}
+// const listDescription = {
+//   color: "#232129",
+//   display: "block",
+//   marginBottom: 8,
+//   padding: 0,
+// }
 
-const siteFooter = {  
-  alignItems: "center",
-  background: "#CFCFCF",
-  boxShadow: "2px 2px 0 0 rgba(0,0,0,1), inset 2px 2px 0 0 rgba(255,255,255,1)",
-  height: 36,
-  display: "flex",
-  width: '100%',
-  position: 'fixed',
-  bottom: 0,
-}
+// const siteFooter = {  
+//   alignItems: "center",
+//   background: "#CFCFCF",
+//   boxShadow: "2px 2px 0 0 rgba(0,0,0,1), inset 2px 2px 0 0 rgba(255,255,255,1)",
+//   height: 36,
+//   display: "flex",
+//   width: '100%',
+//   position: 'fixed',
+//   bottom: 0,
+// }
 
 const titleBar = {
+  alignItems: "center",
   background: "linear-gradient(48deg, rgba(0,28,136,1) 0%, rgba(57,142,203,1) 100%)",
+  color: "white",
+  display: "flex",
   height: 24,
-  margin: "3px 2px 3px 3px"
+  fontFamily: "Arial, sans-serif",
+  fontSize: 14,
+  padding: "0 0 0 8px",
 }
 // const docLinkStyle = {
 //   ...linkStyle,
@@ -91,14 +111,14 @@ const photo = {
   borderRadius: "100%",
 }
 
-const winButton = {
-  borderRadius: 2,
-  boxShadow: "inset 1px 1px 0 0 rgba(255,255,255,1), 1px 1px 0 0 rgba(0,0,0,1)",
-  background: "#CFCFCF",
-  margin: 2,
-  fontSize: 18,
-  fontWeight: 'bold',
-}
+// const winButton = {
+//   borderRadius: 2,
+//   boxShadow: "inset 1px 1px 0 0 rgba(255,255,255,1), 1px 1px 0 0 rgba(0,0,0,1)",
+//   background: "#CFCFCF",
+//   margin: 2,
+//   fontSize: 18,
+//   fontWeight: 'bold',
+// }
 
 const badgeStyle = {
   color: "#fff",
@@ -140,7 +160,34 @@ const jobs = [
     description: "Brought consistency across Sprout Social suite of tools by focusing on redesigning Sprout's social media publishing UX patterns with their new design system.",
     color: "#CC99C9",
     url: "https://sproutsocial.com/",
-    hero: "../images/sproutsocial.png"
+    hero: (
+      <>
+        <StaticImage
+          style={heroImage}
+          src="../images/sprout-1.png"
+          quality={60}
+          alt="Sprout Social"
+        />   
+        <StaticImage
+          style={heroImage}
+          src="../images/sprout-2.png"
+          quality={60}
+          alt="Sprout Social"
+        />     
+        <StaticImage
+          style={heroImage}
+          src="../images/sprout-3.png"
+          quality={60}
+          alt="Sprout Social"
+        />
+        <StaticImage
+          style={heroImage}
+          src="../images/sprout-4.png"
+          quality={60}
+          alt="Sprout Social"
+        />
+      </>
+    ),
   },
   {
     name: "dscout",
@@ -148,124 +195,133 @@ const jobs = [
     description: "Redesigned dscout's research study products for Android and web with new pattern libraries under the same design system.",
     color: "#9EC1CF",
     url: "https://dscout.com/",
-    hero: "../images/dscout.png"
+    hero: (
+      <>
+        <StaticImage
+          style={heroImage}
+          src="../images/dscout-1.png"
+          quality={100}
+          alt="dscout"
+        />   
+        <StaticImage
+          style={heroImage}
+          src="../images/dscout-2.png"
+          quality={100}
+          alt="dscout"
+        />
+        <StaticImage
+          style={heroImage}
+          src="../images/dscout-3.png"
+          quality={100}
+          alt="dscout"
+        />   
+        <StaticImage
+          style={heroImage}
+          src="../images/dscout-4.png"
+          quality={100}
+          alt="dscout"
+        />    
+      </>
+    ),
   },
-  {
-    name: "Threadless",
-    year: "2016",
-    description: "Lead usability testing and ecommerce tools for Artist Shops, Threadless' platform for artists to create and host an online storefront to sell their art printed on custom products fulfilled by Threadless.",
-    color: "#9EE09E",
-    url: "https://threadless.com/",
-    hero: "../images/threadless.png"
-  },
+  // {
+  //   name: "Threadless",
+  //   year: "2016",
+  //   description: "Lead usability testing and ecommerce tools for Artist Shops, Threadless' platform for artists to create and host an online storefront to sell their art printed on custom products fulfilled by Threadless.",
+  //   color: "#9EE09E",
+  //   url: "https://threadless.com/",
+  //   hero: "../images/sprout-3.png",
+  // },
 ]
 
 // markup
 const IndexPage = () => {
   return (
     <>
-    <GlobalStyle />
-    <Helmet>
-      <meta charSet="utf-8" />
-      <meta name="author" content="Jules Simplicio" />
-      <meta name="keywords" content="your, tags" />
-      <meta name="description" content="My personal home on the Web" />
-      <meta name="author" content="Jules Simplicio, hi@julessimplic.io" />
-      <meta name="reply-to" content="hi@julessimplic.io" />
-      <meta name="url" content="http://julessimplic.io" />
-      <html  lang="en" />
-      <title>Jules Simplicio, Design Systems Lead</title>
-    </Helmet>
-    <main style={pageStyles}>
-      <title></title>
-      <section style={sectionStyles}>
-      <div style={titleBar}></div>
-        <article style={articleStyles}>
-          <StaticImage
-            imgStyle={photo}
-            height={120}
-            width={120}
-            src="../images/me.jpg"
-            quality={100}
-            alt="Jules Simplicio headshot"
-          />
-          <h1 style={headingStyles}>
-            Hey, I'm Jules Simplicio. <br /> I design and code. ✌️
-          </h1>
-          <p style={paragraphStyles}>
-            I am building a <a style={linkStyle} href="https://designtokens.app/" title="Design Tokens app">macOS and iOS app</a> that lets people store and track design systems' design tokens. I am building these apps to make accessible tokens and to learn <a style={linkStyle} href="https://developer.apple.com/xcode/swiftui/" title="SwiftUI">SwiftUI</a>.
-          </p>
-          <p style={paragraphStyles}>
-            I work as a product designer and front-end engineer leading design systems for <a style={linkStyle} href="https://www.mozilla.org/en-US/firefox/" title="Firefox">Firefox</a>.
-          </p>
-          <p style={paragraphStyles}>
-            I was born and raised in São Paulo, Brazil, where I fell in love with art through my formative years that were filled with skateboarding sessions, family music gatherings, doodling during class, and playing in a middle school punk rock band.</p>
-          
-          <p>I live with my partner and our two cats in Chicago. I enjoy spinning house music records, playing the guitar, skateboarding, and building mechanical keyboards.
-          </p>
-          <p style={paragraphStyles}>
-            My pronouns are they/he.
-          </p>
-          <p>      
-            📧: <a style={linkStyle} href="mailto:hi@julessimplic.io" title="email">email</a>
-          </p>
-        </article>
-      </section>
+      <GlobalStyle />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="author" content="Jules Simplicio" />
+        <meta name="keywords" content="your, tags" />
+        <meta name="description" content="My personal home on the Web" />
+        <meta name="author" content="Jules Simplicio, hi@julessimplic.io" />
+        <meta name="reply-to" content="hi@julessimplic.io" />
+        <meta name="url" content="http://julessimplic.io" />
+        <html  lang="en" />
+        <title>Jules Simplicio, Design Systems Lead</title>
+      </Helmet>
+      <main style={pageStyles}>
+        <section style={sectionStyles}>
+          <div style={titleBar}>About</div>
+          <article style={articleStyles}>
+            <StaticImage
+              imgStyle={photo}
+              height={120}
+              width={120}
+              src="../images/me.jpg"
+              quality={100}
+              alt="Jules Simplicio headshot"
+            />
+            <h1 style={headingStyles}>
+              I'm Jules Simplicio. I design and code.
+            </h1>
+            <p style={paragraphStyles}>
+              I am building a <a style={linkStyle} href="https://designtokens.app/" title="Design Tokens app">macOS and iOS app</a> that lets people store and track design systems' design tokens. I am building these apps to make accessible tokens and to learn <a style={linkStyle} href="https://developer.apple.com/xcode/swiftui/" title="SwiftUI">SwiftUI</a>.
+            </p>
+            <p style={paragraphStyles}>
+              I work as a product designer and front-end engineer leading design systems for <a style={linkStyle} href="https://www.mozilla.org/en-US/firefox/" title="Firefox">Firefox</a>.
+            </p>
+            <p style={paragraphStyles}>
+              I was born and raised in São Paulo, Brazil, where I fell in love with art through my formative years that were filled with skateboarding sessions, family music gatherings, doodling during class, and playing in a middle school punk rock band.</p>
+            <p style={paragraphStyles}>I live with my partner and our two cats in Chicago. I enjoy spinning house music records, playing the guitar, skateboarding, and building mechanical keyboards.
+            </p>
+            <p style={paragraphStyles}>
+              My pronouns are they/he.
+            </p>
+            <span>      
+              📧: <a style={linkStyle} href="mailto:hi@julessimplic.io" title="email">email</a>
+            </span>
+          </article>
+        </section>
 
-      <section style={sectionStyles}>
-      <div style={titleBar}></div>
+        {jobs.map(job => (
+          <section style={sectionStyles}>
+            <div style={titleBar}>Previously...</div>
+            <article style={articleStyles}>
+              <p style={paragraphStyles}>{job.description}</p>
+              <a
+                style={linkStyle}
+                href={job.url}
+              >
+                {job.year}
+              </a>    
+              <div style={heroImages}>
+                {job.hero}
+              </div>   
+            </article>
+          </section>
+        ))}
 
-        <article style={articleStyles}>
-          <p style={paragraphStyles}>
-            Previously I...
-          </p>
-          <ul style={listStyles}>
-            {jobs.map(job => (
-              <li style={{ ...listItemStyles, color: job.color }}>
-                <p style={listDescription}>{job.description}</p>
-                <a
-                  style={linkStyle}
-                  href={job.url}
-                >
-                  {job.year}
-                </a>           
-              </li>
-            ))}
-          </ul>
-        </article>
-      </section>
-
-      <section style={sectionStyles}>
-      <div style={titleBar}></div>
-
-        <article style={articleStyles}>
-          <p style={paragraphStyles}>
-            Friends and colleagues say...
-          </p>
-          <ul style={listStyles}>
-            {links.map(link => (
-              <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-                <p style={listDescription}>{link.description}</p>              
-                <a
-                  style={linkStyle}
-                  href={link.url}
-                >
-                  {link.text}
-                </a>
-                {link.badge && (
-                  <span style={badgeStyle} aria-label="New Badge">
-                    NEW!
-                  </span>
-                )}
-              </li>
-            ))}
-          </ul>
-        </article>
-      </section>
-      <footer style={siteFooter}>
-        <button style={winButton}>☺</button>
-      </footer>
-    </main>
+        {links.map(link => (
+          <section style={sectionStyles}>
+            <div style={titleBar}>Friends and colleagues say...</div>
+            <article style={articleStyles}>
+              <p style={paragraphStyles}>{link.description}</p>              
+              <a
+                style={linkStyle}
+                href={link.url}
+              >
+                {link.text}
+              </a>
+              {link.badge && (
+                <span style={badgeStyle} aria-label="New Badge">
+                  NEW!
+                </span>
+              )}
+            </article>
+          </section>
+        ))}
+      </main>
     </>
   )
 }
