1189ea506fdc9042b01956d25f5d3a131fda9ed4
jsimplicio <hi@julessimplic.io>
2022-05-23 19:04:15 -0500

    performance enhacement

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,14 @@
 module.exports = (cfg) => {
   cfg.addPassthroughCopy("images");
 };
+
+const Image = require("@11ty/eleventy-img");
+
+(async () => {
+  let img = "/images/ss.webp";
+  let stats = await Image(img, {
+    widths: [300]
+  });
+
+  console.log( stats );
+})();
