a6f2a0546eaed679347329e5dae814be8019699f
jsimplicio <hi@julessimplic.io>
2022-05-23 19:07:30 -0500

    Get rid of bad code

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,3 @@
 module.exports = (cfg) => {
   cfg.addPassthroughCopy("images");
 };
-
-const Image = require("@11ty/eleventy-img");
-
-(async () => {
-  let img = "/images/ss.webp";
-  let stats = await Image(img, {
-    widths: [300]
-  });
-
-  console.log( stats );
-})();
