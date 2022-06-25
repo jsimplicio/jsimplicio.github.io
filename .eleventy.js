141add07b676fd7205af835363aee256b96a2a4a
jsimplicio <hi@julessimplic.io>
2022-06-25 10:27:08 -0500

    Use plugin for img

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,23 @@
+const Image = require("@11ty/eleventy-img");
+
+(async () => {
+  await Image("./images/d-1.jpeg", {
+    formats: ["avif"],
+  });
+
+  await Image("./images/d-2.jpeg", {
+    formats: ["avif"],
+  });
+
+  await Image("./images/ss-1.jpeg", {
+    formats: ["avif"]
+  });
+
+  await Image("./images/ss-2.jpeg", {
+    formats: ["avif"]
+  });
+})();
+
 module.exports = (cfg) => {
-  cfg.addPassthroughCopy("images");
+  cfg.addPassthroughCopy("img");
 };
