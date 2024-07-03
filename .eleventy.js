53c6fce88ff34c28991eedf3c4f63feb2afcb696
jsimplicio <jsimplicio@proton.me>
2024-07-03 09:35:49 -0500

    New font

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts/atkinson.woff2");
   eleventyConfig.addPassthroughCopy("fonts/atkinson-bold.woff2");
+  eleventyConfig.addPassthroughCopy("fonts/young-serif.woff2");
 };
