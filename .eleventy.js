17a477c8a8a40b4ef2130709c157f7539046f89f
jsimplicio <jsimplicio@proton.me>
2024-12-30 12:10:20 -0600

    New font

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets/dscout-1.webp");
+  eleventyConfig.addPassthroughCopy("assets/dscout-2.webp");
+  eleventyConfig.addPassthroughCopy("assets/firefox-1.webp");
+  eleventyConfig.addPassthroughCopy("assets/firefox-2.webp");
+  eleventyConfig.addPassthroughCopy("assets/firefox-3.webp");
+  eleventyConfig.addPassthroughCopy("assets/sprout-social-1.webp");
+  eleventyConfig.addPassthroughCopy("assets/sprout-social-2.webp");
+  eleventyConfig.addPassthroughCopy("fonts/Engrace-Semibold.woff");
   eleventyConfig.addPassthroughCopy("fonts/atkinson.woff2");
   eleventyConfig.addPassthroughCopy("fonts/atkinson-bold.woff2");
-  eleventyConfig.addPassthroughCopy("fonts/young-serif.woff2");
 };
