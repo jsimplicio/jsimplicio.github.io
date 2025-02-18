07e40b59e41e3a283127288e20d432e413970487
jsimplicio <jsimplicio@proton.me>
2025-02-17 21:14:23 -0600

    font

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/firefox-1.webp");
   eleventyConfig.addPassthroughCopy("assets/firefox-2.webp");
   eleventyConfig.addPassthroughCopy("assets/firefox-3.webp");
+  eleventyConfig.addPassthroughCopy("assets/firefox-4.webp");
+  eleventyConfig.addPassthroughCopy("assets/firefox-5.webp");
   eleventyConfig.addPassthroughCopy("assets/sprout-social-1.webp");
   eleventyConfig.addPassthroughCopy("assets/sprout-social-2.webp");
-  eleventyConfig.addPassthroughCopy("fonts/Engrace-Semibold.woff");
+  eleventyConfig.addPassthroughCopy("fonts/Gambarino-Regular.woff2");
   eleventyConfig.addPassthroughCopy("fonts/atkinson.woff2");
   eleventyConfig.addPassthroughCopy("fonts/atkinson-bold.woff2");
 };
