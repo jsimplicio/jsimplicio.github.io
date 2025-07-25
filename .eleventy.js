edbb8d3de3a1cb78c5e6649a9535f0153c86df06
jsimplicio <jsimplicio@proton.me>
2025-07-24 22:04:24 -0500

    tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/firefox-2.webp");
   eleventyConfig.addPassthroughCopy("assets/firefox-3.webp");
   eleventyConfig.addPassthroughCopy("assets/firefox-4.webp");
-  eleventyConfig.addPassthroughCopy("assets/firefox-5.webp");
   eleventyConfig.addPassthroughCopy("assets/sprout-social-1.webp");
   eleventyConfig.addPassthroughCopy("assets/sprout-social-2.webp");
   eleventyConfig.addPassthroughCopy("fonts/Gambarino-Regular.woff2");
